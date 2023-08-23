e128c386cb43caf36538609fa7e13dfb5b8e80ae
Guusbumps <82210545+Guusbumps@users.noreply.github.com>
2023-08-23 17:31:01 +0200

    created routes, started genes svg

---
+++ src/routes/genes/+page.svelte
@@ -0,0 +1,30 @@
+<h1>Guus Spenkelink - UHasselt - 2056061</h1>
+
+<!-- menu -->
+<div>
+<a href="/">Home</a>;
+<a href="/interactions">Interactions</a>
+</div>
+
+<script>
+    const width = 600;
+    const height = 600;
+    const r = 250;
+    const margin = { top: 5, right: 5, bottom: 5, left: 5 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+  </script>
+  
+  <svg viewBox='0 0 {width} {height}'>
+    <g transform="translate({margin.left}, {margin.top})">
+      <circle cx={innerWidth / 2} cy={innerHeight / 2} r={r} stroke="black" stroke-width=1 fill=white/>
+    </g>
+  </svg>
+  
+  <style>
+    svg {
+      border: 1px solid black;
+      border-radius: 5px;
+    }
+  </style>
+ 
---
+++ src/routes/interactions/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Guus Spenkelink - UHasselt - 2056061</h1>
+
+<!-- menu -->
+<div>
+<a href="/">Home</a>;
+<a href="/genes">Genes</a>
+</div>
