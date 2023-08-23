7a78250c19a410f1f321a7e7cfc4871359a293c1
Guusbumps <82210545+Guusbumps@users.noreply.github.com>
2023-08-23 17:54:51 +0200

    1 file modified

--- src/routes/genes/+page.svelte
+++ src/routes/genes/+page.svelte
@@ -7,14 +7,16 @@
 </div>
 
 <script>
-    const width = 600;
-    const height = 600;
-    const r = 250;
-  </script>
+  const width = 600;
+  const height = 600;
+  const r = 250;
+  
+  
+</script>
   
   <svg viewBox='0 0 {width} {height}'>
-      <circle cx={width / 2} cy={height / 2} r={r} stroke="black" stroke-width=1 fill=white/>
-      
+      <circle cx={width / 2} cy={height / 2} r={r}/>
+      <line x1={width / 2} y1= {width / 2} x2="40" y2="50" />
   </svg>
   
   <style>
@@ -22,5 +24,14 @@
       border: 1px solid black;
       border-radius: 5px;
     }
+    line {
+                stroke: black;
+                stroke-width: 3;
+        }
+    circle {
+                stroke: black;
+                stroke-width: 1;
+                fill: white;
+        }
   </style>
  
