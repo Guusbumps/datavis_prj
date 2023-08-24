bbf93fcc1c19a5549a8b2597bfb916f18c48f2ae
Guusbumps <82210545+Guusbumps@users.noreply.github.com>
2023-08-24 22:16:40 +0200

    1 file modified

--- src/routes/genes/[slug]/+page.svelte
+++ src/routes/genes/[slug]/+page.svelte
@@ -15,27 +15,30 @@
 </div>
 
 <script>
-  let datapoints = [];
+  import { onMount } from 'svelte';
+  let datapoints2 = [];
+  onMount(() => {
   fetch('https://vda-lab.github.io/assets/genes.json')
     .then(res => res.json())
-    .then(data2 => datapoints = data2)
-
+    .then(data2 => datapoints2 = data2)
+  })
+  
   export let data
   // let data_for_slug = datapoints.filter((d) => { return d.gene_name == data.slug})[0];
   
-
   function isName(gene) {
     return gene.gene_name == data.slug;
   }
 
-  let d_f_s = datapoints.filter(isName)[0];
+  let d_f_s = datapoints2.filter(isName)[0];
 
 </script>
 
 <h1>{data.slug}</h1>
-{JSON.stringify(datapoints[0])}
+{datapoints2[0]}
+
 <ul>
-  {#each datapoints as d, i}
+  {#each datapoints2 as d, i}
   <li>{d}</li>
   {/each}
 </ul>
