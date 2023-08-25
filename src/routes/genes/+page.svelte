7e41623ed97f1772a41ea37381c7070473832a2c
Guusbumps <82210545+Guusbumps@users.noreply.github.com>
2023-08-25 22:28:56 +0200

    1 file modified

--- src/routes/genes/+page.svelte
+++ src/routes/genes/+page.svelte
@@ -30,41 +30,15 @@
 	let gene_opacity = 0.5;
   function handleMouseOver(d) {
     selected_datapoint = d.gene_name;
-		gene_color = 'red';
-    gene_opacity = 1.0;
+    // gene_color = 'red';
+    // gene_opacity = 1.0;
 	}
 	function handleMouseOut(d) {
     selected_datapoint = "";
-		gene_color = 'black';
-    gene_opacity = 0.5;
+		// gene_color = 'black';
+    // gene_opacity = 0.5;
 	}
 </script>
-
-<svg viewBox='0 0 {width} {height}'>
-    <circle cx={width / 2} cy={height / 2} r={r}/>
-
-    {#each datapoints as d, i}
-      {#if d.gene_strand === "forward"}
-        <line x1={r-10} y1=0 x2={r} y2=0
-          color = {gene_color}
-          opacity = {gene_opacity}
-          transform="translate({width/2},{height/2}) rotate({360*i/datapoints.length})"
-          on:mouseover={handleMouseOver(d)}
-          on:mouseout={handleMouseOut(d)} 
-          on:click={handleClick(d)}
- />
-      {:else if d.gene_strand === "reverse"}
-        <line x1={r} y1=0 x2={r+10} y2=0
-          transform="translate({width/2},{height/2}) rotate({360*i/datapoints.length})" 
-          on:mouseover={function() {selected_datapoint = d.gene_name}}
-          on:mouseout={function() {selected_datapoint = ""}}
-          on:click={handleClick(d)} />
-      {/if}
-    {/each}
-    <text x={width/2} y={height/2} font-size=20>
-      {selected_datapoint}
-    </text>
-</svg>
 
 <style>
   svg {
@@ -76,9 +50,38 @@
               stroke-width: 0.33;
               opacity: 0.5;
       }
+  line.selected {
+              stroke: red;
+              stroke-width: 1;
+              opacity: 1;
+  }
   circle {
               stroke: black;
               stroke-width: 1;
               fill: white;
       }
 </style>
+
+<svg viewBox='0 0 {width} {height}'>
+    <circle cx={width / 2} cy={height / 2} r={r}/>
+
+    {#each datapoints as d, i}
+      {#if d.gene_strand === "forward"}
+        <line x1={r-10} y1=0 x2={r} y2=0
+          transform="translate({width/2},{height/2}) rotate({360*i/datapoints.length})"
+          on:mouseover={handleMouseOver(d)}
+          on:mouseout={handleMouseOut(d)}
+          on:click={handleClick(d)}
+          class:selected="{selected_datapoint && d.gene_name == selected_datapoint}" />
+      {:else if d.gene_strand === "reverse"}
+        <line x1={r} y1=0 x2={r+10} y2=0
+          transform="translate({width/2},{height/2}) rotate({360*i/datapoints.length})"
+          on:mouseover={handleMouseOver(d)}
+          on:mouseout={handleMouseOut(d)} 
+          on:click={handleClick(d)} />
+      {/if}
+    {/each}
+    <text x={width/2} y={height/2} font-size=20>
+      {selected_datapoint}
+    </text>
+</svg>
