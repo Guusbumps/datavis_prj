6b11c6fb5aa791605810fcecbf578bc82256208f
Guusbumps <82210545+Guusbumps@users.noreply.github.com>
2023-08-27 08:14:37 +0200

    1 file modified

--- src/routes/interactions/[slug]/+page.js
+++ src/routes/interactions/[slug]/+page.js
@@ -1,5 +1,19 @@
-export const load = ({ params }) => {
+// export const load = ({ params }) => {
+//   return {
+//       slug: params.slug
+//   }
+// }
+
+export const load = ({ params, fetch }) => {
+
+  const fetchInteractions = async () => {
+    const res = await fetch("https://vda-lab.github.io/assets/interactions.json")
+    const data = await res.json()
+    return data
+  }
+
   return {
-      slug: params.slug
+    slug: params.slug,
+    interactions: fetchInteractions()
   }
 }
