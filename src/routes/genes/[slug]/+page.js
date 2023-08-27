7ac0e3d21912a37792c4b405add35e947c6b6ac0
Guusbumps <82210545+Guusbumps@users.noreply.github.com>
2023-08-27 08:35:12 +0200

    1 file modified

--- src/routes/genes/[slug]/+page.js
+++ src/routes/genes/[slug]/+page.js
@@ -1,5 +1,11 @@
-export const load = ({ params }) => {
+export const load = ({ params, fetch }) => {
+  const fetchGenes = async () => {
+    const res = await fetch("https://vda-lab.github.io/assets/genes.json")
+    const data = await res.json()
+    return data
+  }
   return {
-      slug: params.slug
+      slug: params.slug,
+      genes: fetchGenes()
   }
 }
